324876845621ee9384516e05995535372bd7a2ad
sharmachait <chait8126@gmail.com>
2024-05-25 00:05:10 +0530

    start

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface env {}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		console.log(request.body);
+		console.log(request.headers);
+		console.log(request);
+
+		if (request.method == 'GET') {
+			return Response.json({
+				message: 'get request',
+			});
+		} else {
+			return Response.json({
+				message: 'not get',
+			});
+		}
 	},
 };
